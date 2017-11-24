0c30d5b1bc2be3b28f73d3ada256cbca02c7e53c
Patrick Telnoni <Patrick Telnoni>
2017-11-24 16:25:39 +0700

    Adding uploads folder and two sample images

--- putrest.php
+++ putrest.php
@@ -13,13 +13,13 @@
       $data = array();
       $response['status']          = 200;
       $response['status_message']  = "Data Found";
-
       $i=0;
       while ($row = $result->fetch_assoc()) {
         $data[$i]['nama']   = $row['nama'];
         $data[$i]['nim']    = $row['nim'];
         $data[$i]['prodi']  = $row['programstudi'];
         $data[$i]['ipk']    = $row['ipk'];
+        $data[$i]['foto']   = $row['foto'];
         $i++;
       }
       $response['data']=$data;
@@ -61,11 +61,12 @@
 
 	$request_method=$_SERVER["REQUEST_METHOD"];
   header("Content-Type:application/json");
-  header("HTTP/1.1 200 ". "Perfectly Fine");
+
 	switch($request_method)
 	{
 		case 'GET':
 			// Retrive Products
+      header("HTTP/1.1 200 ". "Perfectly Fine");
 			if(!empty($_GET["mahasiswa_id"]))
 			{
 				$product_id=intval($_GET["product_id"]);
@@ -77,21 +78,24 @@
 			}
 			break;
 		case 'POST':
+      header("HTTP/1.1 200 ". "Perfectly Fine");
 			// Insert Product
       insert_mahasiswa();
 			break;
 		case 'PUT':
 			// Update Product
+      header("HTTP/1.1 200 ". "Perfectly Fine");
       $mahasiswa_id=intval($_GET["mahasiswa_id"]);
 			update_mahasiswa($mahasiswa_id);
 			break;
 		case 'DELETE':
 			// Delete Product
+      header("HTTP/1.1 200 ". "Perfectly Fine");
 			$mahasiswa_id=intval($_GET["mahasiswa_id"]);
 			delete_product($mahasiswa_id);
 			break;
 		default:
 			// Invalid Request Method
-			header("HTTP/1.0 405 Method Not Allowed");
+			header("HTTP/1.1 405 Method Not Allowed");
 			break;
 	}
